c6f2ce373cdd5db3baca3be1c3c1e1f6d0a4b760
delomeo <debartolomeo.francesco@gmail.com>
2025-06-29 12:58:53 +0200

    refactor: move disparity calculation in stereoobject.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2 as cv
 import re
 import matplotlib.pyplot as plt
-from stereoobject import ImageLR, show_image_pairs
+from stereoobject import ImageLR, show_image_pairs, compute_disparity
 from utils import read_image, read_disparity
 
     
@@ -46,51 +46,9 @@ def main():
     # Pack an LR StereoObject
     artroomLR = ImageLR(imgL, imgR)
 
-    # Stereo SGBM Parameters
-    min_disp = 0
-    num_disp = 16 * 15  # Needs to be divisible by 16
-    window_size = 7  # Must be odd
-
-    # Create StereoSGBM object
-    # Note: The parameters here are just an example, you may need to adjust them based
-    stereo = cv.StereoSGBM_create(
-        minDisparity=min_disp,
-        numDisparities=num_disp,
-        blockSize=window_size,
-        P1=8 * 3 * window_size ** 2,
-        P2=32 * 3 * window_size ** 2,
-        disp12MaxDiff=9,
-        uniquenessRatio=10,
-        speckleWindowSize=100,
-        speckleRange=32,
-        preFilterCap=63
-    )
-    
     print('computing disparity...')
-    disp = stereo.compute(artroomLR.left, artroomLR.right).astype(np.float32) / 32.0
+    compute_disparity(artroomLR)
 
     
-    h, w = imgL.shape[:2]
-
-    f = 0.8*w     
-    
-                         # guess for focal length
-    Q = np.float32([[1, 0, 0, -0.5*w],
-                    [0,-1, 0,  0.5*h], # turn points 180 deg around x-axis,
-                    [0, 0, 0,     -f], # so that y-axis looks up
-                    [0, 0, 1,      0]])
-    
-    points = cv.reprojectImageTo3D(disp, Q)
-    colors = cv.cvtColor(imgL, cv.COLOR_BGR2RGB)
-    mask = disp > disp.min()
-    out_points = points[mask]
-    out_colors = colors[mask]
-
-    cv.imshow('left', imgL)
-    cv.imshow('disparity', (disp-min_disp)/num_ disp)
-    cv.waitKey()
-
-    print('Done')
-
 if __name__ == "__main__":
     main()
--- stereoobject.py
+++ stereoobject.py
@@ -9,6 +9,39 @@ class ImageLR:
     left: np.array
     right: np.array
 
+def compute_disparity(image_pairs: ImageLR, min_disp: int = 0, num_disp: int = 16 * 15, window_size: int = 7) -> np.array:
+    """
+    Computes the disparity map using StereoSGBM.
+
+    :param image_pairs: An ImageLR object containing left and right images.
+    :param min_disp: Minimum disparity value.
+    :param num_disp: Number of disparities (must be divisible by 16).
+    :param window_size: Size of the block window (must be odd).
+    :return: Disparity map as a numpy array.
+    """
+    # Check if type is correct
+    if not isinstance(image_pairs, ImageLR): 
+        raise TypeError("Image Pairs must always be passed as a Stereo Object!")
+    
+    # Create StereoSGBM object
+    stereo = cv.StereoSGBM_create(
+        minDisparity=min_disp,
+        numDisparities=num_disp,
+        blockSize=window_size,
+        P1=8 * 3 * window_size ** 2,
+        P2=32 * 3 * window_size ** 2,
+        disp12MaxDiff=1,
+        uniquenessRatio=10,
+        speckleWindowSize=100,
+        speckleRange=32,
+        preFilterCap=63
+    )
+    
+    # Compute disparity map
+    disparity = stereo.compute(image_pairs.left, image_pairs.right).astype(np.float32) / 16.0
+    
+    return disparity
+
 def show_image_pairs(image_pairs: ImageLR) -> None:
     """
     Displays a pair of images side by side.
