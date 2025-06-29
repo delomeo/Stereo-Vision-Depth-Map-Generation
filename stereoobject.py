4b00e2cdd254f29e8e3e1621daa60ce7da9bf03d
delomeo <debartolomeo.francesco@gmail.com>
2025-06-29 18:30:34 +0200

    refactor: move compute_disparity as method of tuner

--- stereoobject.py
+++ stereoobject.py
@@ -9,42 +9,6 @@ class ImageLR:
     left: np.array
     right: np.array
 
-def compute_disparity(image_pairs: ImageLR, **params) -> np.array:
-    """
-    Computes the disparity map using StereoSGBM.
-
-    :param image_pairs: An ImageLR object containing left and right images.
-    :param params: Parameters for StereoSGBM.
-    :return: Disparity map as a numpy array.
-    :raises TypeError: If image_pairs is not an instance of ImageLR.
-    """
-    # Check if type is correct
-    if not isinstance(image_pairs, ImageLR): 
-        raise TypeError("Image Pairs must always be passed as a Stereo Object!")
-    
-    # Create StereoSGBM object
-    stereo = cv.StereoSGBM_create(
-        minDisparity=params.get('minDisparity', 0),
-        numDisparities=params.get('numDisparities', 16),
-        blockSize=params.get('blockSize', 5),
-        P1=params.get('P1', 8 * 3 * 5 ** 2),
-        P2=params.get('P2', 32 * 3 * 5 ** 2),
-        disp12MaxDiff=params.get('disp12MaxDiff', 1),   
-        uniquenessRatio=params.get('uniquenessRatio', 10),
-        speckleWindowSize=params.get('speckleWindowSize', 50),
-        speckleRange=params.get('speckleRange', 1),
-        mode=cv.STEREO_SGBM_MODE_SGBM_3WAY if params.get('mode', 'SGBM_3WAY') == 'SGBM_3WAY' else cv.STEREO_SGBM_MODE_SGBM
-    )
-
-    print("Using StereoSGBM with parameters:")
-    # Get parameters from StereoSGBM object
-    for key, value in stereo.getParams().items():
-        print(f"{key}: {value}")
-    
-    # Compute disparity map
-    disparity = stereo.compute(image_pairs.left, image_pairs.right).astype(np.float32) / 16.0
-    
-    return disparity
 
 def show_image_pairs(image_pairs: ImageLR) -> None:
     """
